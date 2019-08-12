b7314aeb06f3b5515469ba9d2bb514e4323980cd
Your Name <you@example.com>
2019-08-12 16:57:18 -0400

    provided data with CartContext

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import ProductContext from './Contexts/ProductContext'
+import CartContext from './Contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -19,21 +20,23 @@ function App() {
 	return (
 		
 	<ProductContext.Provider value={{products, addItem}}>
-		<div className="App">
-			<Navigation cart={cart} />
+		<CartContext.Prodiver value={cart}>
+			<div className="App">
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				component={Products}
-			/>
+				{/* Routes */}
+				<Route
+					exact
+					path="/"
+					component={Products}
+				/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
+			</div>
+		</CartContext.Prodiver>
 	</ProductContext.Provider>
 	);
 }
