5bd5e83841a46f083f092ea82643bcff58562221
Your Name <you@example.com>
2019-08-12 16:32:31 -0400

    created ProductContext and imported into App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from './Contexts/ProductContext'
 
 function App() {
 	const [products] = useState(data);
@@ -16,6 +17,7 @@ function App() {
 	};
 
 	return (
+		
 		<div className="App">
 			<Navigation cart={cart} />
 
